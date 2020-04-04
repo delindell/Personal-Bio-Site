f1321eef13c2a14c88fc5f1e7be965932b28a740
delindell <daviselindell@gmail.com>
2020-04-04 03:18:08 -0500

    personal site deployed with projects printing in JSON files, need to work on styling some

--- src/javascripts/components/projectPrinter.js
+++ src/javascripts/components/projectPrinter.js
@@ -1,21 +1,29 @@
-import projects from '../helpers/data/projectData';
+import projectData from '../helpers/data/projectData';
 import utils from '../helpers/utils';
 
 const createProjectCards = () => {
+  console.log('hellow');
   let domString = '';
-  projects.getProjects().forEach((project) => {
-    if (project.available !== false) {
-      domString += '<div class="card projectCards col-5 text-center">';
-      domString += `<h3 class="card-title">${project.title}</h3>`;
-      domString += `<img class="card-img-top project-img" src="${project.screenshot}" alt="Card image cap">`;
-      domString += '<div class="card-body justify-content-center">';
-      domString += `<p class="card-text">${project.description}</p>`;
-      domString += `<a class="btn btn-primary mt-2" id="gitHubBtn" href="${project.githubUrl}"><i class="fab fa-github"></i><a>`;
+  projectData.getProjects()
+    .then((projects) => {
+      console.log(projects);
+      domString += '<div class="project-container d-flex flex-wrap justify-content-center">';
+      projects.forEach((project) => {
+        if (project.available !== false) {
+          domString += '<div class="card projectCards col-3 text-center">';
+          domString += `<h3 class="card-title">${project.title}</h3>`;
+          domString += `<img class="card-img-top project-img" src="${project.screenshot}" alt="Card image cap">`;
+          domString += '<div class="card-body justify-content-center">';
+          domString += `<p class="card-text">${project.description}</p>`;
+          domString += `<a class="btn btn-primary mt-2" id="gitHubBtn" href="${project.githubUrl}"><i class="fab fa-github"></i><a>`;
+          domString += '</div>';
+          domString += '</div>';
+        }
+      });
       domString += '</div>';
-      domString += '</div>';
-    }
-    utils.printToDom('projectsPage', domString);
-  });
+      utils.printToDom('projectsPage', domString);
+    // .catch((err) => console.error('lost boards', err));
+    });
 };
 
 export default { createProjectCards };
--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,51 +1,73 @@
-const projects = [
-  {
-    title: 'Tamagotchi',
-    screenshot: 'https://github.com/delindell/tamagotchi/blob/master/screenshots/tamagotchiScreenshot.png?raw=true',
-    description: 'In this project we built upon our previous knowledge of using ES6 modules to create a Tamagotchi. The Tamagotchi has several buttons that increase or decrease the score corresponding to the button pressed. This was the first project in which we used SASS to style. With the SASS I created several variables for colors, and other various styling methods.', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML5, SCSS, Vanilla JavaScript, Version Control with Github, ES6 Modules, JQuery',
-    available: true,
-    url: 'https://github.com/delindell/tamagotchi',
-    githubUrl: 'https://github.com/delindell/tamagotchi',
-  },
-  {
-    title: 'Chatty Messanger',
-    screenshot: 'https://github.com/delindell/chatty-cobralily/raw/master/screenshots/light-mode.png',
-    description: 'Chatty Messanger is an app that allows seperate users to message one another, delete messages, search and post gifs, all on this online forum type page.', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML5, SCSS, Vanilla JavaScript, Version Control with Github, ES6 Modules, JQuery',
-    available: true,
-    url: 'https://github.com/delindell/chatty-cobralily',
-    githubUrl: 'https://github.com/delindell/chatty-cobralily',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-];
+// const projects = [
+//   {
+//     title: 'Tamagotchi',
+//     screenshot: 'https://github.com/delindell/tamagotchi/blob/master/screenshots/tamagotchiScreenshot.png?raw=true',
+//     description: 'In this project we built upon our previous knowledge of using ES6 modules to create a Tamagotchi. The Tamagotchi has several buttons that increase or decrease the score corresponding to the button pressed. This was the first project in which we used SASS to style. With the SASS I created several variables for colors, and other various styling methods.', // A good project description includes 'the what', 'the why', and 'the how'.
+//     technologiesUsed: 'HTML5, SCSS, Vanilla JavaScript, Version Control with Github, ES6 Modules, JQuery',
+//     available: true,
+//     url: 'https://github.com/delindell/tamagotchi',
+//     githubUrl: 'https://github.com/delindell/tamagotchi',
+//   },
+//   {
+//     title: 'Chatty Messanger',
+//     screenshot: 'https://github.com/delindell/chatty-cobralily/raw/master/screenshots/light-mode.png',
+//     description: 'Chatty Messanger is an app that allows seperate users to message one another, delete messages, search and post gifs, all on this online forum type page.', // A good project description includes 'the what', 'the why', and 'the how'.
+//     technologiesUsed: 'HTML5, SCSS, Vanilla JavaScript, Version Control with Github, ES6 Modules, JQuery',
+//     available: true,
+//     url: 'https://github.com/delindell/chatty-cobralily',
+//     githubUrl: 'https://github.com/delindell/chatty-cobralily',
+//   },
+//   {
+//     title: 'Cool Project',
+//     screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+//     description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+//     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+//     available: false,
+//     url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+//     githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+//   },
+//   {
+//     title: 'Cool Project',
+//     screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+//     description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+//     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+//     available: false,
+//     url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+//     githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+//   },
+//   {
+//     title: 'Cool Project',
+//     screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+//     description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+//     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+//     available: false,
+//     url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+//     githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+//   },
+// ];
 
-const getProjects = () => projects;
+// const getProjects = () => projects;
+
+// export default { getProjects };
+
+
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getProjects = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/projects.json`)
+    .then((response) => {
+      const demProjects = response.data;
+      const projects = [];
+      Object.keys(demProjects).forEach((projectId) => {
+        demProjects[projectId].id = projectId;
+        projects.push(demProjects[projectId]);
+      });
+      resolve(projects);
+    })
+    .catch((err) => reject(err));
+});
 
 export default { getProjects };
