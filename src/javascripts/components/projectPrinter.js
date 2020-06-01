8191024a014f052e4b0b5e4cc6eca1c36d0d8958
delindell <daviselindell@gmail.com>
2020-06-01 17:52:21 -0500

    wip

--- src/javascripts/components/projectPrinter.js
+++ src/javascripts/components/projectPrinter.js
@@ -2,15 +2,13 @@ import projectData from '../helpers/data/projectData';
 import utils from '../helpers/utils';
 
 const createProjectCards = () => {
-  console.log('hellow');
   let domString = '';
   projectData.getProjects()
     .then((projects) => {
-      console.log(projects);
-      domString += '<div class="project-container d-flex flex-wrap justify-content-center">';
+      domString += '<div class="project-container d-flex flex-wrap justify-content-between">';
       projects.forEach((project) => {
         if (project.available !== false) {
-          domString += '<div class="card projectCards col-3 text-center">';
+          domString += '<div class="card projectCards col-3 text-center d-flex justify-content-center">';
           domString += `<h3 class="card-title">${project.title}</h3>`;
           domString += `<img class="card-img-top project-img" src="${project.screenshot}" alt="Card image cap">`;
           domString += '<div class="card-body justify-content-center">';
@@ -22,8 +20,8 @@ const createProjectCards = () => {
       });
       domString += '</div>';
       utils.printToDom('projectsPage', domString);
-    // .catch((err) => console.error('lost boards', err));
-    });
+    })
+    .catch((err) => console.error('project cards have been lost', err));
 };
 
 export default { createProjectCards };
