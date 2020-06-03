7d4359d896a0b0a80cddbb58c79d07864d859ada
delindell <daviselindell@gmail.com>
2020-06-02 19:59:31 -0500

    projects deployed

--- src/javascripts/components/projectPrinter.js
+++ src/javascripts/components/projectPrinter.js
@@ -5,11 +5,11 @@ const createProjectCards = () => {
   let domString = '';
   projectData.getProjects()
     .then((projects) => {
-      domString += '<div class="project-container d-flex flex-wrap justify-content-between">';
+      domString += '<div class="project-container d-flex flex-wrap justify-content-around">';
       projects.forEach((project) => {
         if (project.available !== false) {
-          domString += '<div class="card projectCards col-3 text-center d-flex justify-content-center">';
-          domString += `<h3 class="card-title">${project.title}</h3>`;
+          domString += '<div class="card projectCards col-4 text-center">';
+          domString += `<h2 class="card-title">${project.title}</h2>`;
           domString += `<img class="card-img-top project-img" src="${project.screenshot}" alt="Card image cap">`;
           domString += '<div class="card-body justify-content-center">';
           domString += `<p class="card-text">${project.description}</p>`;
